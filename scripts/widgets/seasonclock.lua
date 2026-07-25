fc3a32fec1dc7df1d00722a8bb1a6dcd58bbf194
Rafael Lizarralde <rezecib@gmail.com>
2026-07-25 10:30:45 -0700

    Fix crash in IA aporkalypse compatibility

--- scripts/widgets/seasonclock.lua
+++ scripts/widgets/seasonclock.lua
@@ -361,7 +361,7 @@ function SeasonClock:OnSeasonLengthsChanged(data)
 	end
 	-- asgerrr: for IA-side practical reasons, the pre-aporkalypse season is not networked, so something else must be done. i also happen to prefer that the clock shows something useful during aporkalypse
 	if self._dst and TheWorld.state.isaporkalypse then
-		for season, _ in data do
+		for season, _ in pairs(data) do
 			if season ~= "aporkalypse" then data[season] = 0 end
 		end
 	end
